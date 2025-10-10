a44627819508e1e3be72d58ea62ae20f7a2c8e61
mfrendi <mfrendiriyannanda_2327240161@mhs.mdp.ac.id>
2025-10-10 17:01:33 +0700

    pertemuan 9

--- toko-online/app.js
+++ toko-online/app.js
@@ -5,6 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var engine = require('ejs-blocks');// menggunkkan ejs block
 
+require("./app_toko_online/models/db");
+
 var indexRouter = require('./app_toko_online/routes/index');
 var usersRouter = require('./app_toko_online/routes/users');
 var productRouter = require('./app_toko_online/routes/product');
